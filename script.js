40686dd48e84089219b23ae1b94bc2b435bea807
Sasha Mainardi <sashamainardi4@gmail.com>
2022-01-20 17:04:32 +0100

    snack 2 completed

--- script.js
+++ script.js
@@ -8,7 +8,8 @@
 // Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 // Generare numeri random al posto degli 0 nelle proprietà:
 // Punti fatti e falli subiti.
-// Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+// Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo 
+// nomi e falli subiti e stampiamo tutto in console.
 // BONUS
 // Stampare in pagina oltre che in console!
 
@@ -59,4 +60,64 @@ console.log(message);
 
 
 // SNACK 2 
+
+const getRandomNumber = (min, max) => {
+    const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    return randomNumber;
+
+}
+
+const teamList = [
+    {
+        nome: "manchester",
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: "liverpool",
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: "arsenal",
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: "juventus",
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+]
+
+for (let i = 0; i < teamList.length; i++) {
+    let { puntiFatti } = teamList[i];
+    let { falliSubiti } = teamList[i];
+
+    // console.log(puntiFatti);
+    // console.log(falliSubiti);
+
+    puntiFatti = getRandomNumber(1, 30);
+    falliSubiti = getRandomNumber(1, 50);
+    teamList[i].puntiFatti = puntiFatti;
+    teamList[i].falliSubiti = falliSubiti;
+    console.log(teamList[i]);
+}
+
+// let nome = "";
+// let falliSubiti = "";
+const newArray = [];
+
+for (let i = 0; i < teamList.length; i++) {
+    const { nome } = teamList[i];
+    const { falliSubiti } = teamList[i];
+    const newObject = {
+        nome: nome,
+        falliSubiti: falliSubiti
+    }
+
+    newArray.push(newObject);
+}
+
+console.log(newArray);
 
